9ae7969293264052bbd802b6ad28029ce2c2aa7b
Amine Bouramtane <abouramt@e2r7p2.1337.ma>
2023-11-10 09:42:26 +0100

    same changes

--- ft_split.c
+++ ft_split.c
@@ -2,6 +2,19 @@
 
 #include "libft.h"
 
+
+char    *count_alpha(char   *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+		while ()
+        i++;
+    }
+}
+
 char **ft_split(char const *s, char c)
 {
     
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: abouramt <abouramt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 15:35:25 by abouramt          #+#    #+#             */
-/*   Updated: 2023/11/09 22:51:29 by abouramt         ###   ########.fr       */
+/*   Updated: 2023/11/10 09:39:33 by abouramt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,6 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-// 	size_t	i;
-// 	size_t	j;
-
-// 	j = 0;
-// 	i = ft_strlen(dst);
-// 	if (dstsize < i || dstsize == 0)
-// 		return (ft_strlen(src) + dstsize);
-
-// 	while (src[j] && i + j + 1 < dstsize)
-// 	{
-// 		dst[i + j] = src[j];
-// 		j++;
-// 	}
-// 	if (i < dstsize)
-// 		dst[i + j] = '\0';
-// 	return (ft_strlen(dst) + ft_strlen(src));
-// }
-
 	size_t	len_dst;
 	size_t	len_src;
 	size_t	i;
