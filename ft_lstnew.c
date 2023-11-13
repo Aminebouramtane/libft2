04a6efb8acb68b151db51e65025267637e3e849a
Amine Bouramtane <abouramt@e1r8p8.1337.ma>
2023-11-13 04:36:58 +0100

    add the first page of bonus

---
+++ ft_lstnew.c
@@ -0,0 +1,16 @@
+
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*my_node;
+
+	my_node = (t_list *)malloc(sizeof(t_list));
+	if (my_node == NULL)
+		return (NULL);
+	my_node->content = content;
+	my_node->next = NULL;
+
+	return (my_node);
+}
